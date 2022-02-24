9d1ba4c37806b266c4196858cc668c376bf1c2b8
Jari Van Melckebeke <jarivanmelckebeke@gmail.com>
2022-02-24 11:53:22 +0100

    opdracht2

--- labo1-opdracht1/Validators/WineValidator.cs
+++ labo1-opdracht1/Validators/WineValidator.cs
@@ -8,6 +8,8 @@ public class WineValidator : AbstractValidator<Wine>
     public WineValidator()
     {
         RuleFor(p => p.Name).NotEmpty().MinimumLength(5).WithMessage("Naam moet minstens 5 characters lang zijn");
-        RuleFor(p => p.Year).GreaterThan(1900).LessThan(2022).WithMessage("Ongeldig jaar");
+        RuleFor(p => p.Year)
+            .GreaterThan(1900).WithMessage("Ongeldig jaar")
+            .LessThan(2022).WithMessage("ongeldig jaar");
     }
 }

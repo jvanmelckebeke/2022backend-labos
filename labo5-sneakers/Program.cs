05cf391dfa0133c144b5aaad2c8953b3f0b326c6
Jari Van Melckebeke <jarivanmelckebeke@gmail.com>
2022-03-17 14:48:30 +0100

    setup labo

--- labo5-sneakers/Program.cs
+++ labo5-sneakers/Program.cs
@@ -1,16 +1,19 @@
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using labo5_sneakers.Configuration;
+using labo5_sneakers.Context;
 using labo5_sneakers.Models;
 using labo5_sneakers.Repositories;
 using labo5_sneakers.Services;
-using labo5_sneakers.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var dbsettings = builder.Configuration.GetSection("MongoConnection");
 
 builder.Services.Configure<DatabaseSettings>(dbsettings);
+
+
+builder.Services.AddTransient<IMongoContext, MongoContext>();
 
 builder.Services.AddTransient<IBrandRepository, BrandRepository>();
 builder.Services.AddTransient<ISneakerRepository, SneakerRepository>();
@@ -20,22 +23,30 @@ builder.Services.AddTransient<IOrderRepository, OrderRepository>();
 builder.Services.AddTransient<ISneakerService, SneakerService>();
 builder.Services.AddFluentValidation(fv =>
     fv.RegisterValidatorsFromAssemblyContaining<SneakerRepository>());
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.MapSwagger();
+app.UseSwaggerUI();
+
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/brands", async (ISneakerService service) => { return Results.Ok(await service.GetBrands()); });
+app.MapGet("/setup", async (ISneakerService service) =>
+{
+    await service.SetupData();
+    return Results.Created("/brands", new {Message = "we have setted up zi data"});
+});
 
-app.MapGet("/occasions", async (ISneakerService service) => { return Results.Ok(await service.GetOccasions()); });
+app.MapGet("/brands", async (ISneakerService service) => Results.Ok(await service.GetBrands()));
 
-app.MapGet("/sneakers", async (ISneakerService service) => { return Results.Ok(await service.GetSneakers()); });
+app.MapGet("/occasions", async (ISneakerService service) => Results.Ok(await service.GetOccasions()));
+
+app.MapGet("/sneakers", async (ISneakerService service) => Results.Ok(await service.GetSneakers()));
 
 app.MapGet("/sneakers/{sneakerId}",
-    async (ISneakerService service, string sneakerId) =>
-    {
-        return Results.Ok(await service.GetSneakerBySneakerId(sneakerId));
-    });
+    async (ISneakerService service, string sneakerId) => Results.Ok(await service.GetSneakerBySneakerId(sneakerId)));
 
 app.MapPost("/sneakers", async (ISneakerService service, IValidator<Sneaker> sneakerValidator, Sneaker sneaker) =>
 {
@@ -43,11 +54,11 @@ app.MapPost("/sneakers", async (ISneakerService service, IValidator<Sneaker> sne
 
     if (validationResult.IsValid)
     {
-        return Results.Created("/registrations", service.AddSneaker(sneaker));
+        return Results.Created("/registrations", await service.AddSneaker(sneaker));
     }
 
     var errors = validationResult.Errors.Select(x => new {errors = x.ErrorMessage});
     return Results.BadRequest(errors);
 });
 
-app.Run();
+app.Run("http://localhost:3000");
